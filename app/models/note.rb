870cd87f83bf47b8769e29888280735beddab6e8
BradGillis4 <bradgillis4@gmail.com>
2021-04-15 02:06:23 -0400

    commit tree 521cecddf127677b062f947613099aaca1d2d171

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,12 @@
 class Note < ActiveRecord::Base
   # add associations here
+  belongs_to :song
 end
+
+
+# Genre
+# has a name attribute (String)
+# has many songs
+# Note
+# has content attribute (String)
+# belongs to a Song
